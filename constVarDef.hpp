b99e4d9e2b312f61e04d3dbe81f58437a2fbcd3e
Xieyu Wang <varianovsky@outlook.com>
2022-01-30 19:41:49 +0800

    update structure program

---
+++ constVarDef.hpp
@@ -0,0 +1,2 @@
+#pragma once
+const std::string VERSION="0.0.1";
---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include "constVarDef.hpp"
+int main(int argc,char*argv[]){
+    std::cout<<"version: "<<VERSION;
+    //here to execute interface..
+}
